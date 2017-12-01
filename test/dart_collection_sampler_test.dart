04172a0df83244efdfb1f35b1fa78df46f20ff68
Steve Christensen <stevec1973@gmail.com>
2017-12-01 03:07:49 -0700

    start adding tests

--- test/dart_collection_sampler_test.dart
+++ test/dart_collection_sampler_test.dart
@@ -1,13 +1,32 @@
 import 'package:dart_collection_sampler/dart_collection_sampler.dart';
+import 'package:dart_dice_parser/dart_dice_parser.dart';
+
 import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'dart:math';
+
+class MockRandom extends Mock implements Random {}
 
 void main() {
-  group('A group of tests', () {
+  var mockRandom = new MockRandom();
+
+  group('dice sampler', () {
+
+    var roller = new DiceRoller(mockRandom);
+    var diceParser = new DiceParser(roller);
 
     setUp(() {
     });
 
     test('First Test', () {
+      var sampler = new DiceSampler(diceParser);
+
+      when(mockRandom.nextInt(argThat(inInclusiveRange(1, 1000)))).thenReturn(1);
+
+      // nextInt returns 1, dice roller adds 1... 2+2=4
+      expect(sampler.pick("2d6", [1,2,3,4,5,6,7,8,9,10,11,12]), equals(4));
+      expect(sampler.pick("2d6", ["a","b","c","d","e"]), equals("d"));
     });
   });
 }
